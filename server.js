8133f300689e8b33663f01dd98d4e3fd38508551
Zeeshan-ai <zeeshan.ultracodes@gmail.com>
2023-04-13 15:04:28 +0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
-
-
+const authorRouter = require('./routes/authors')
+// const bodyParser = require('body-parser')
  
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(express.urlencoded({limit: '10mb', extended: false}))
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/libraryManagement', {
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
 }) 
 const db = mongoose.connection;
@@ -23,7 +23,8 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('connected to mongoose db'))
 
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
+ 
 app.listen(process.env.PORT || 3000, () => { 
-    console.log(`Server running on Port: ${process.env.PORT}`)
-})    
+    console.log(`Server running on Port: ${process.env.PORT}`) 
+})     
